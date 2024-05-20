cf43f43f50b4a96b3cdb7b6f5244237f64586cdb
Ivan Saker <68342806+iskr889@users.noreply.github.com>
2024-05-20 03:55:30 +0000

    Add files via upload

---
+++ main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "../../utils/src/servidor.h"
+#include "../../utils/src/utils.h"
+
+typedef struct {
+    String puerto_escucha;
+    String tam_memoria;
+    String tam_pagina;
+    String path_intrucciones;
+    String retardo_respuesta;
+} t_memoria_config;
+
+typedef enum {
+    SET,
+    IO_GEN_SLEEP,
+    EXIT
+} t_tipo_instruccion;
+
+typedef struct {
+    t_tipo_instruccion tipo;
+    char registro[10]; // Para instrucciones tipo SET
+    int valor;         // Para instrucciones tipo SET
+    char dispositivo[20]; // Para instrucciones tipo IO_GEN_SLEEP
+    int tiempo;        // Para instrucciones tipo IO_GEN_SLEEP
+} t_instruccion;
+
+
+/**
+* @fn    Carga la configuración en la estructura memoria_config
+* @brief Con el archivo config abierto solicita memoria y lo carga en la estructura memoria_config
+*/
+t_memoria_config* load_memoria_config(t_config* config);
+
+#endif
