804afbac9ccce262fbb24bc5e76620071151421f
Ivan Saker <ivansaker88@gmail.com>
2024-06-14 01:35:55 +0000

    Update stdout.c

--- stdout.c
+++ stdout.c
@@ -17,76 +17,78 @@ void interfaz_stdout(String nombre){
     enviar_nombre_interfaz(nombre, conexion_kernel);
 
     while(1)
-        recibir_direccion(conexion_memoria);
+        stdout_recibir_direccion(conexion_kernel, conexion_memoria);
 
     close(conexion_kernel);
     close(conexion_memoria);
 }
 
-void io_stdout_write(char* direccion){
-    
-    // Mostramos la dirección recibida por pantalla
-    printf("Dirección recibida: %s\n", direccion);
-    //Respetamos el tiempo unidad de trabajo
-    TIEMPO_UNIDAD_DE_TRABAJO(interfaz_config->tiempo_u_trabajo);
-}
+void io_stdout_write(int direccion, int cant_caracteres, int fd_memoria){
 
-void stdout_recibir_direccion(int fd_memoria){
+    int total_size = 2 * sizeof(int);
+
+    payload_t *buffer = payload_create(total_size);
+
+    // cant_direcciones
+    payload_add(buffer, &direccion, sizeof(int));
+    payload_add(buffer, &cant_caracteres, sizeof(int));
+
+    paquete_t *paquete_a_enviar = crear_paquete(IO_STDOUT_WRITE, buffer);
     
+    if(enviar_paquete(fd_memoria, paquete_a_enviar) != OK){
+        
+        log_error(extra_logger, "No se pudo enviar el paquete a memoria");
+        
+        payload_destroy(buffer);
+        liberar_paquete(paquete_a_enviar);
+    }
+
     paquete_t *paquete = recibir_paquete(fd_memoria);
 
     if(paquete == NULL)
         exit(EXIT_FAILURE);
     if(paquete->operacion != IO_STDOUT_WRITE){
-        log_error(extra_logger, "Error en recibir la direccion de memoria");
+        log_error(extra_logger, "Error en recibir el paquete de memoria");
         return;
     }
     
     //leemos el paquete
-    char* direccion = malloc(10 * sizeof(char));
-    payload_read(paquete->payload, direccion, 10 * sizeof(char));
+    char* texto = payload_read_string(paquete->payload);
+    // Mostramos la dirección recibida por pantalla
+    printf("Texto recibido de memoria: %s\n", texto);
+    //Respetamos el tiempo unidad de trabajo
+    TIEMPO_UNIDAD_DE_TRABAJO(interfaz_config->tiempo_u_trabajo);
 
-    //La mando para mostrar por pantalla
-    io_stdout_write(direccion);
-
-    //Libero memoria
-    free(direccion);
     payload_destroy(paquete->payload);
     liberar_paquete(paquete);
 }
 
-// KERNEL
-void send_io_stdout_write(int socket) {
+void stdout_recibir_direccion(int fd_kernel, int fd_memoria){
+    
+    paquete_t *paquete = recibir_paquete(fd_kernel);
 
-    int cant_caracteres = 12;
-    int cant_direcciones = 4;
-    int direcciones[] = {88, 56, 45, 22};
-
-    int tam = sizeof(int) + cant_direcciones * sizeof(int) + sizeof(int);
-
-    payload_t *payload = payload_create(tam);
-
-    // cantidad de direcciones
-    payload_add(payload, &cant_direcciones, sizeof(uint32_t));
-
-    // direcciones
-    for(int i = 0; i < cant_direcciones; i++) {
-        payload_add(payload, &direcciones[i], sizeof(int));
-    }
-
-    // cantidad de caracteres
-    payload_add(payload, &cant_caracteres, sizeof(int));
-
-    paquete_t *paquete = crear_paquete(IO_STDIN_READ, payload);
-
-    if(enviar_paquete(socket, paquete) != OK){
-        printf("fallo al enviar el paquete a stdout");
+    if(paquete == NULL)
         exit(EXIT_FAILURE);
+    if(paquete->operacion != IO_STDOUT_WRITE){
+        log_error(extra_logger, "Error operacion invalida");
+        return;
     }
+    
+    //leemos el paquete
+    int direccion;
+    int cant_caracteres;
+    payload_read(paquete->payload, &direccion, sizeof(int));
+    payload_read(paquete->payload, &cant_caracteres, sizeof(int));
 
-    payload_destroy(payload);
+
+    //La mando para mostrar por pantalla
+    io_stdout_write(direccion, cant_caracteres, fd_memoria);
+
+    //Libero memoria
+    payload_destroy(paquete->payload);
     liberar_paquete(paquete);
 }
+
 
 
 
